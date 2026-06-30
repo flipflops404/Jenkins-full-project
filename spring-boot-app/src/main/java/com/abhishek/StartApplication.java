46ace1c106472ecb9983133b1980c94ec596d7e3
flipflops404 <boomergr@protonmail.com>
2026-06-30 12:04:20 +0300

    Update StartApplication.java

--- spring-boot-app/src/main/java/com/abhishek/StartApplication.java
+++ spring-boot-app/src/main/java/com/abhishek/StartApplication.java
@@ -12,8 +12,8 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "I have successfuly built a sprint boot application using Maven");
-        model.addAttribute("msg", "This application is deployed on to Kubernetes using Argo CD");
+        model.addAttribute("title", "John Was Here");
+        model.addAttribute("msg", "So was Rashed & Abdallah x_x");
         return "index";
     }
 
